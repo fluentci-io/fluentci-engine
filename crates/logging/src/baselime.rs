ba879f58744e15b099223d7872b443fbe0b83c63
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-27 07:46:03 +0000

    add duration to log events

--- crates/logging/src/baselime.rs
+++ crates/logging/src/baselime.rs
@@ -15,6 +15,8 @@ pub enum Level {
 #[derive(Serialize, Default)]
 pub struct Payload {
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub duration: Option<u128>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub message: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub error: Option<String>,
@@ -71,6 +73,58 @@ impl Baselime {
                         git_commit_hash: Some(env::var("GIT_COMMIT_HASH").unwrap_or_default()),
                         git_remote_url: Some(env::var("GIT_REMOTE_URL").unwrap_or_default()),
                         git_author: Some(env::var("GIT_AUTHOR").unwrap_or_default()),
+                        ..Default::default()
+                    }])?,
+                };
+                self.client
+                    .post(URL)
+                    .header("x-api-key", api_key)
+                    .header("x-service", "fluentci-core")
+                    .header("Content-Type", "application/json")
+                    .body(json)
+                    .send()?;
+                Ok(())
+            }
+            Err(e) => Err(Error::msg(format!("BASELIME_API_KEY not set: {}", e)))?,
+        }
+    }
+
+    pub fn send_with_duration(
+        &self,
+        data: String,
+        level: Level,
+        namespace: String,
+        duration: u128,
+    ) -> Result<(), Error> {
+        match env::var("BASELIME_API_KEY") {
+            Ok(api_key) => {
+                let json = match level {
+                    Level::Info => serde_json::to_string(&vec![Payload {
+                        message: Some(data),
+                        request_id: Uuid::new_v4().to_string(),
+                        namespace,
+                        git_commit_message: Some(
+                            env::var("GIT_COMMIT_MESSAGE").unwrap_or_default(),
+                        ),
+                        git_branch: Some(env::var("GIT_BRANCH").unwrap_or_default()),
+                        git_commit_hash: Some(env::var("GIT_COMMIT_HASH").unwrap_or_default()),
+                        git_remote_url: Some(env::var("GIT_REMOTE_URL").unwrap_or_default()),
+                        git_author: Some(env::var("GIT_AUTHOR").unwrap_or_default()),
+                        duration: Some(duration),
+                        ..Default::default()
+                    }])?,
+                    Level::Error => serde_json::to_string(&vec![Payload {
+                        error: Some(data.to_string()),
+                        request_id: Uuid::new_v4().to_string(),
+                        namespace,
+                        git_commit_message: Some(
+                            env::var("GIT_COMMIT_MESSAGE").unwrap_or_default(),
+                        ),
+                        git_branch: Some(env::var("GIT_BRANCH").unwrap_or_default()),
+                        git_commit_hash: Some(env::var("GIT_COMMIT_HASH").unwrap_or_default()),
+                        git_remote_url: Some(env::var("GIT_REMOTE_URL").unwrap_or_default()),
+                        git_author: Some(env::var("GIT_AUTHOR").unwrap_or_default()),
+                        duration: Some(duration),
                         ..Default::default()
                     }])?,
                 };
--- crates/logging/src/lib.rs
+++ crates/logging/src/lib.rs
@@ -38,3 +38,20 @@ pub fn info(data: &str, namespace: &str) -> Result<(), Error> {
     }
     Ok(())
 }
+
+pub fn info_with_duration(data: &str, namespace: &str, duration: u128) -> Result<(), Error> {
+    if let Ok(_) = env::var("BASELIME_API_KEY") {
+        let data = data.to_string();
+        let namespace = namespace.to_string();
+        thread::spawn(move || {
+            let baselime_client = baselime::new();
+            match baselime_client.send_with_duration(data, Level::Info, namespace, duration) {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+            }
+        });
+    }
+    Ok(())
+}
