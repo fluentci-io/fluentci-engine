729aec68df5e903279adcc34e6e400b8b102cc68
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-28 10:45:11 +0000

    [sdk/typescript] fix lodash export

--- sdk/typescript/deps.ts
+++ sdk/typescript/deps.ts
@@ -3,5 +3,5 @@ export { gql };
 export { ClientError, GraphQLClient } from "npm:graphql-request@6.1.0";
 import * as env from "jsr:@tsirysndr/env-js@0.1.2";
 export { env };
-import * as _ from "npm:lodash@4.17.21";
+import _ from "npm:lodash@4.17.21";
 export { _ };
