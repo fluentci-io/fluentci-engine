9420ad9fa464876041c80557ea17e3af8a3ef5b8
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-13 20:16:10 +0000

    update example

--- sdk/purescript/demo/src/Main.purs
+++ sdk/purescript/demo/src/Main.purs
@@ -7,7 +7,9 @@ import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Class.Console as Console
 import FluentCI.Client (dag, pipeline, setSecret)
-import FluentCI.Pipeline (withSecretVariable, withExec, stdout)
+import FluentCI.Mise (withWorkdir)
+import FluentCI.Mise as Mise
+import FluentCI.Pipeline (mise, withSecretVariable, withExec, stdout)
 import FluentCI.Secret (plaintext)
 
 main :: Effect Unit
@@ -19,6 +21,18 @@ main = launchAff_ do
     p2 <- withSecretVariable p1 "GITHUB" secret
     withExec p2 ["echo", "$GITHUB"]
   output <- stdout p
+
+  m <- liftEffect $ do
+    pip <- pipeline dag "mise-demo"
+    m1 <- mise pip
+    m2 <- withWorkdir m1 "./mise-demo"
+    m3 <- Mise.withExec m2 ["mise", "--version"]
+    Mise.withExec m3 ["which", "bun"]
+  miseOutput <- Mise.stdout m
+  
   Console.log value
   Console.log "Secret demo:"
   Console.log output
+
+  Console.log "Mise: "
+  Console.log miseOutput
